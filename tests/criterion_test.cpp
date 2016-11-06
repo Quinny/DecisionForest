7aafffdfd7f666ea1334e981bccd453c1921e09c
Quinn Perfetto <thequinnftw@gmail.com>
2016-11-06 09:12:56 -0500

    criterion test added

--- tests/criterion_test.cpp
+++ tests/criterion_test.cpp
@@ -10,3 +10,17 @@ TEST_F(CriterionTest, ZeroImpurity) {
   EXPECT_EQ(elements_impurity.first, 10);
   EXPECT_EQ(elements_impurity.second, 0);
 }
+
+TEST_F(CriterionTest, NonZeroImpurity) {
+  std::map<int, std::size_t> label_histogram{{1, 3}, {4, 7}, {5, 2}};
+  auto elements_impurity = qp::rf::gini_impurity(label_histogram);
+
+  const double expected_size = 12;
+  const double expected_impurity =
+      ((3 / expected_size) * (1 - (3 / expected_size))) +
+      ((7 / expected_size) * (1 - (7 / expected_size))) +
+      ((2 / expected_size) * (1 - (2 / expected_size)));
+
+  EXPECT_EQ(expected_size, elements_impurity.first);
+  EXPECT_DOUBLE_EQ(expected_impurity, elements_impurity.second);
+}
