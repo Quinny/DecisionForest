11957a79296fdb8f395f3ae74021180dc07cc384
Quinn Perfetto <thequinnftw@gmail.com>
2016-11-08 13:22:09 -0500

    improved threadpool, should speed things up

--- threadpool.h
+++ threadpool.h
@@ -8,11 +8,8 @@
 namespace qp {
 namespace threading {
 
-template <typename ReturnValue>
 class Threadpool {
  public:
-  using WorkType = std::function<ReturnValue()>;
-
   // Starts a thread pool with the number of threads available on the machine.
   Threadpool() : Threadpool(std::thread::hardware_concurrency()) {}
 
@@ -20,8 +17,10 @@ class Threadpool {
   // creating too many threads will have adverse performance benefits.
   Threadpool(int n_threads);
 
-  // Add work to the queue.  Returns a future to the results.
-  std::future<ReturnValue> add(const WorkType& work);
+  // Add work to the queue.  Returns a future contain the results.
+  template <typename F, typename... Args>
+  auto add(F&& f, Args&&... args) ->
+      typename std::future<typename std::result_of<F(Args...)>::type>;
 
   // Shuts down the threadpool.  All tasks currently being executed will finish
   // and all threads will be joined.  All tasks still in the queue will be
@@ -31,7 +30,7 @@ class Threadpool {
  private:
   std::vector<std::thread> threads_;
   bool shutdown_ = false;
-  std::queue<std::pair<WorkType, std::promise<ReturnValue>>> work_queue_;
+  std::queue<std::function<void()>> work_queue_;
   std::mutex mu_;
   std::condition_variable work_added_;
 
@@ -39,56 +38,57 @@ class Threadpool {
   void worker();
 };
 
-template <typename T>
-Threadpool<T>::Threadpool(int n_threads) {
+Threadpool::Threadpool(int n_threads) {
   for (int i = 0; i < n_threads; ++i) {
     threads_.emplace_back(&Threadpool::worker, this);
   }
 }
 
-template <typename T>
-std::future<T> Threadpool<T>::add(const WorkType& work) {
-  std::lock_guard<std::mutex> lock(mu_);
-  work_queue_.emplace(work, std::promise<T>{});
+template <typename F, typename... Args>
+auto Threadpool::add(F&& f, Args&&... args) ->
+    typename std::future<typename std::result_of<F(Args...)>::type> {
+  using ReturnType = typename std::result_of<F(Args...)>::type;
+
+  auto work = std::make_shared<std::packaged_task<ReturnType()>>(
+      std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+  std::future<ReturnType> ret = work->get_future();
+
+  {
+    std::lock_guard<std::mutex> lock(mu_);
+    work_queue_.emplace([work]() { (*work)(); });
+  }
+
   work_added_.notify_one();
-  return work_queue_.back().second.get_future();
+  return ret;
 }
 
-// TODO - Does not work when T is void.
-template <typename T>
-void Threadpool<T>::worker() {
+void Threadpool::worker() {
   while (true) {
-    std::unique_lock<std::mutex> lock(mu_);
-    if (shutdown_) {
-      return;
+    std::function<void()> work;
+
+    {
+      std::unique_lock<std::mutex> lock(mu_);
+      work_added_.wait(lock,
+                       [this] { return shutdown_ || !work_queue_.empty(); });
+      if (shutdown_) return;
+
+      work = std::move(work_queue_.front());
+      work_queue_.pop();
     }
 
-    if (work_queue_.empty()) {
-      work_added_.wait(lock, [&] { return !work_queue_.empty() || shutdown_; });
-    } else {
-      auto work_item = std::move(work_queue_.front());
-      work_queue_.pop();
-      lock.unlock();
-      const auto result = work_item.first();
-      work_item.second.set_value(result);
-    }
+    work();
   }
 }
 
-template <typename T>
-Threadpool<T>::~Threadpool() {
+Threadpool::~Threadpool() {
   mu_.lock();
   shutdown_ = true;
-  work_added_.notify_all();
   mu_.unlock();
 
+  work_added_.notify_all();
   for (auto& thread : threads_) {
     thread.join();
-  }
-
-  while (!work_queue_.empty()) {
-    work_queue_.front().second.set_exception(std::exception_ptr{});
-    work_queue_.pop();
   }
 }
 
